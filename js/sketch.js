6ec96eab3914df4af192e8cb97c88e899a7726ef
emily clark <93390989+emix-y@users.noreply.github.com>
2021-11-12 16:45:57 +0000

    added p5 code for smiley face

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,39 @@
+var canvasSize = 400; 
+
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(canvasSize,canvasSize);
     canvas.parent("p5container");
 }
 
 function draw() {
-    background(220);
+    var eyeX = map(mouseX,0,canvasSize,125,175);
+    var eyeY = map(mouseY,0,canvasSize,120,160);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+   // console.log(mouseX,mouseY)
+
+    background(225);
+
+    stroke(100)
+    strokeWeight(4)
+    fill(255, 255, 0);
+    ellipse(width / 2,height / 2, 300);
+
+    strokeWeight(4)
+    fill(255);
+    ellipse(eyeX,eyeY,45,60)
+    ellipse(eyeX +100,eyeY,45,60)
+
+
+    fill(255,215,0)
+    strokeWeight(0)
+    ellipse(300,200,50)
+    ellipse(100,200,50)
+
+    stroke(100)
+    strokeWeight(4)
+
+    noFill()
+    arc(200,220,180,180,radians(30),radians(150));
+
 }
